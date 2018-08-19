3d10b34342d2ad150a0aca40961685b63b310941
Tanveer Bashir <tan.m.bashir@gmail.com>
2018-08-19 19:31:40 -0400

    remove duplicate images from the bestseller list

--- screens/ScreenBook.js
+++ screens/ScreenBook.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import {FlatList, StyleSheet, Text, View, TouchableOpacity, Button, Image} from 'react-native'
+import { FlatList, StyleSheet, Text, View, TouchableOpacity, Button, Image } from 'react-native'
 import Row from '../Components/Row'
-import {List} from 'react-native-elements'
+import { List } from 'react-native-elements'
 import fetchList from '../Network/Api'
 import Devider from '../Components/devider'
 import { Color } from '../src/Color';
 
 
 export default class ScreenBook extends React.Component {
-  static navigationOptions = ({navigation}) => ({
+  static navigationOptions = ({ navigation }) => ({
     headerTitle: navigation.getParam('list'),
     headerTintColor: Color.white,
     headerStyle: {
@@ -23,7 +23,7 @@ export default class ScreenBook extends React.Component {
 
   getBooks = async (list) => {
     const response = await fetchList(list)
-    this.setState({books: [...response.data.results.books]})
+    this.setState({ books: [...response.data.results.books] })
   }
 
   componentDidMount() {
@@ -36,12 +36,12 @@ export default class ScreenBook extends React.Component {
 
   render() {
     return (
-      <List containerStyle={{borderTopWidth: 0, borderBottomWidth: 0, marginTop: 0,}}>
+      <List containerStyle={{ borderTopWidth: 0, borderBottomWidth: 0, marginTop: 0, }}>
         <FlatList
           data={this.state.books}
-          renderItem={({item}) => <Row {...item} updated={this.props.navigation.getParam('updated')}/>}
+          renderItem={({ item }) => <Row {...item} updated={this.props.navigation.getParam('updated')} />}
           ItemSeparatorComponent={this.renderDevider}
-          keyExtractor={item => item.title}/>
+          keyExtractor={item => item.title} />
       </List>
     )
   }
